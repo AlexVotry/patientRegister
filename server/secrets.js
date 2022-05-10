c9e9342ca8a48667cadb9a3e99d3f97bf9c601ce
Alex Votry <alexvotry@gmail.com>
2022-05-10 10:00:37 -0700

    Delete secrets.js
    remove file

--- server/secrets.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://AlexDone:CtMmus4LdeREMCj@cluster0.epczc.mongodb.net/doneApp?retryWrites=true&w=majority',
-    cookieKey: 'aslkjidf;lskd',
-    db_Name: 'doneApp'
-}
